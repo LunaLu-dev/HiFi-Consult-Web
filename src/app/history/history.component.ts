18acfbcac7c11ddf42f631d1b8a00d5b060bfd50
Luna Nordbergh <dev@lunalu.org>
2024-10-26 18:19:03 +0200

    update

---
+++ src/app/history/history.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-history',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.css'
+})
+export class HistoryComponent {
+
+}
